7140d57e58c4656e11931074f8fe573b0d50bde1
Wesly Giovano <weslygio@yahoo.com>
2023-02-12 22:15:43 +0700

    change buffer binding method

--- src/script.js
+++ src/script.js
@@ -1,3 +1,10 @@
+var objectList = [];
+var maxNumVertices = 200;
+var positionBuffer;
+var colorBuffer;
+
+var objectSelectElement = document.getElementById('object-select');
+
 window.onload = function init() {
     /** @type {HTMLCanvasElement} */
     const canvas = document.querySelector('#glcanvas');
@@ -25,74 +32,111 @@ window.onload = function init() {
         }
     };
 
-    const objectInfo = initObject(gl);
-
     gl.useProgram(programInfo.program);
 
-    // Bind buffers to send to GPU
-    gl.bindBuffer(gl.ARRAY_BUFFER, objectInfo.positions);
+    // Make buffers to hold everything
+    positionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, 8*maxNumVertices, gl.STATIC_DRAW);
     gl.vertexAttribPointer(programInfo.attribLocations.vertexPosition, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(programInfo.attribLocations.vertexPosition);
 
-    gl.bindBuffer(gl.ARRAY_BUFFER, objectInfo.colors);
+    colorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, 16*maxNumVertices, gl.STATIC_DRAW);
     gl.vertexAttribPointer(programInfo.attribLocations.vertexColor, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(programInfo.attribLocations.vertexColor);
+
+    // For testing purpose
+    initObject();
+    initObject2();
 
     function render() {
         // Reset canvas
         gl.clearColor(0.8, 0.8, 0.8, 1.0);
         gl.clear(gl.COLOR_BUFFER_BIT);
-        drawObject(gl, programInfo, objectInfo);
+        drawObject(gl);
         requestAnimationFrame(render);
     }
     requestAnimationFrame(render);      // Buat animation nanti
 }
 
 /* --- Draw Object --- */
-function drawObject(gl, programInfo, objectInfo) {
-    if (objectInfo.type === "square") {
-        gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
+function drawObject(gl) {
+    var vc = 0;
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    for (var i = 0; i < objectList.length; i++) {
+        const object = objectList[i];
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+        gl.bufferSubData(gl.ARRAY_BUFFER, 8*vc, new Float32Array(object.positions));
+        gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+        gl.bufferSubData(gl.ARRAY_BUFFER, 16*vc, new Float32Array(object.colors));
+
+        if (object.type === "square") {
+            gl.drawArrays(gl.TRIANGLE_FAN, vc, 4);
+        }
+
+        vc += object.positions.length;
     }
 }
 
-/* --- Save and Load --- */
-function initObject(gl) {
-    // var positions = [
-    //     -0.5, -0.5, 
-    //     0.5, -0.5, 
-    //     0.5, 0.5, 
-    //     -0.5, 0.5
-    // ];
+// For testing purpose
+function initObject() {
+    var positions = [
+        -0.5, -0.5, 
+        0.5, -0.5, 
+        0.5, 0.5, 
+        -0.5, 0.5
+    ];
 
-    // var colors = [
-    //     1.0, 0.0, 0.0, 1.0,
-    //     0.0, 1.0, 0.0, 1.0,
-    //     0.0, 0.0, 1.0, 1.0,
-    //     1.0, 0.0, 1.0, 1.0
-    // ];
-    
-    var positions = [];
-    var colors = [];
+    var colors = [
+        1.0, 0.0, 0.0, 1.0,
+        0.0, 1.0, 0.0, 1.0,
+        0.0, 0.0, 1.0, 1.0,
+        1.0, 0.0, 1.0, 1.0
+    ];
 
-    const positionBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-    
-    const colorBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
-    
-    return {
+    var object = {
         type: "square",
-        positions: positionBuffer,
-        colors: colorBuffer,
+        positions: positions,
+        colors: colors
     };
+
+    objectList.push(object);
 }
 
+function initObject2() {
+    var positions = [
+        0.7, 0.7, 
+        0.7, 0.8, 
+        0.8, 0.8,
+        0.8, 0.7 
+    ];
+
+    var colors = [
+        1.0, 0.0, 0.0, 1.0,
+        0.0, 1.0, 0.0, 1.0,
+        0.0, 0.0, 1.0, 1.0,
+        1.0, 0.0, 1.0, 1.0
+    ];
+
+    var object = {
+        type: "square",
+        positions: positions,
+        colors: colors
+    };
+
+    objectList.push(object);
+}
 
 /* --- Transformations --- */
-function translate(obj) {
-
+function translate(obj, x, y) {
+    positions = [];
+    for (let i = 0; i < obj.positions.length; i+=2) {
+        obj.positions[i] += +x;
+        obj.positions[i+1] += +y;
+    }
 }
 
 function dilate(obj) {
